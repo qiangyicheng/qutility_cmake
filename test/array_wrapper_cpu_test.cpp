a62bdb356bbb82823e8be9d4e9908527291ff3cc
Yicheng Qiang <qiangyicheng@icloud.com>
2022-05-31 05:11:47 -0400

    improve a simple array_wrapper test

--- test/array_wrapper_cpu_test.cpp
+++ test/array_wrapper_cpu_test.cpp
@@ -4,12 +4,10 @@
 #include <gtest/gtest.h>
 
 // current project header
-#include "qutility/c_array.h"
 #include "qutility/array_wrapper/array_wrapper_cpu.h"
 
-TEST(DummyTest, DummyTest0)
+TEST(ArrayWrapperCPU, DArrayHBWTrivial)
 {
-    EXPECT_EQ(0, 0);
-
-    qutility::array_wrapper::DArrayHBW<double> arr(128);
+    qutility::array_wrapper::DArrayHBW<double> arr(0., 128);
+    EXPECT_DOUBLE_EQ(0., arr[0]);
 }
