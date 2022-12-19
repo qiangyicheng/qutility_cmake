60c7de78b8d986099cfd7c95ddbe330fbf1592f5
Yicheng Qiang <yicheng.qiang@ds.mpg.de>
2022-12-19 15:17:03 +0100

    fix some bugs on arm

--- include/qutility/array_wrapper/internal/cpu/hbw.h
+++ include/qutility/array_wrapper/internal/cpu/hbw.h
@@ -1,6 +1,11 @@
 #pragma once
 
+#include "boost/predef.h"
+
 #include "base.h"
+
+#if BOOST_ARCH_X86
+
 #include "hbw_posix_allocator.h"
 
 namespace qutility {
@@ -12,3 +17,18 @@ namespace qutility {
 		using ArrayHBW = ArrayCPU<T, hbw::allocator<T, A>, A>;
 	}
 }
+
+#elif BOOST_ARCH_ARM
+
+namespace qutility {
+	namespace array_wrapper {
+
+		template <class T, std::size_t A = 64>
+		using DArrayHBW = ArrayCPU<T, boost::alignment::aligned_allocator<T, A>, A>;
+
+		template <class T, std::size_t A = 64>
+		using ArrayHBW = ArrayCPU<T, boost::alignment::aligned_allocator<T, A>, A>;
+	}
+}
+
+#endif
