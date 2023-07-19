bff6595411ac4defa8bec5ed2f17a3cc7a1fa759
Yicheng Qiang <qiangyicheng@icloud.com>
2023-07-19 14:42:00 +0200

    add exit_with_message for string

--- include/qutility/message/error_message.h
+++ include/qutility/message/error_message.h
@@ -1,10 +1,17 @@
 #pragma once
 
 #include<iostream>
+#include<string>
 
 namespace qutility {
 	namespace message {
 		inline void exit_with_message(char const* const message, char const* const file, int const line) {
+			std::cerr << "Error at " << file << ":" << line << ":\n"
+				<< message << std::endl;
+			exit(1);
+		}
+
+		inline void exit_with_message(const std::string & message, char const* const file, int const line) {
 			std::cerr << "Error at " << file << ":" << line << ":\n"
 				<< message << std::endl;
 			exit(1);
