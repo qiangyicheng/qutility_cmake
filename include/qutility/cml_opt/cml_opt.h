8104bb4df0e1c17da95a0aacc951bce00f76cfae
Yicheng Qiang <qiangyicheng@icloud.com>
2023-08-09 16:17:54 +0200

    add required option

--- include/qutility/cml_opt/cml_opt.h
+++ include/qutility/cml_opt/cml_opt.h
@@ -16,7 +16,7 @@ namespace qutility
 {
     namespace cml_opt
     {
-        class SimOptionsBase
+        class OptionsBase
         {
         public:
             using vm_T = boost::program_options::variables_map;
@@ -30,8 +30,8 @@ namespace qutility
             static std::string to_string(const std::string &str) { return str; }
         };
 
-        template<typename SimOptionsDerivedT, typename = typename std::enable_if_t<std::is_base_of_v<SimOptionsBase,SimOptionsDerivedT>>>
-        void obtain_cml_options(int argc, const char * const * argv, SimOptionsDerivedT &opts)
+        template <typename OptionsDerivedT, typename = typename std::enable_if_t<std::is_base_of_v<OptionsBase, OptionsDerivedT>>>
+        void obtain_cml_options(int argc, const char *const *argv, OptionsDerivedT &opts)
         {
             namespace po = boost::program_options;
             try
@@ -58,102 +58,121 @@ namespace qutility
     }
 }
 
-#ifndef QUTILITY_SIM_REGISTER_OPTIONS
+#ifndef QUTILITY_REGISTER_OPTION
 
-#define QUTILITY_SIM_REGISTER_OPTIONS_VALUE_AND_FUNCTIONS(type, name, tag, default, desc) \
-public:                                                                                   \
-    type name;                                                                            \
-                                                                                          \
-protected:                                                                                \
-    constexpr static const char *name##_tag = tag;                                        \
-    constexpr static const char *name##_desc = desc;                                      \
-    const type name##_default = default;                                                  \
-    void name##_get_func(const vm_T &vm)                                                  \
-    {                                                                                     \
-        name = vm[name##_tag].as<type>();                                                 \
-    }                                                                                     \
-    void name##_register_func(osd_T &osd) const                                           \
-    {                                                                                     \
-        boost::shared_ptr<od_T> opt(new od_T(                                             \
-            name##_tag,                                                                   \
-            boost::program_options::value<type>()->default_value(name##_default),         \
-            name##_desc));                                                                \
-        osd.add(opt);                                                                     \
-    }                                                                                     \
-    std::pair<std::string, std::string> name##_str_func() const                           \
-    {                                                                                     \
-        return {name##_tag, to_string(name)};                                             \
-    }                                                                                     \
-    void name##_json_insert_func(jsobject_T &obj) const                                   \
-    {                                                                                     \
-        obj[name##_tag] = name;                                                           \
+#define QUTILITY_REGISTER_OPTION_IMPL_VALUE_AND_COMMON_FUNCTIONS(type, name, tag) \
+public:                                                                           \
+    type name;                                                                    \
+                                                                                  \
+protected:                                                                        \
+    constexpr static const char *name##_tag = tag;                                \
+    void name##_get_func(const vm_T &vm)                                          \
+    {                                                                             \
+        name = vm[name##_tag].as<type>();                                         \
+    }                                                                             \
+    std::pair<std::string, std::string> name##_str_func() const                   \
+    {                                                                             \
+        return {name##_tag, to_string(name)};                                     \
+    }                                                                             \
+    void name##_json_insert_func(jsobject_T &obj) const                           \
+    {                                                                             \
+        obj[name##_tag] = name;                                                   \
     }
 
-#define QUTILITY_SIM_REGISTER_OPTIONS_STUB(class_name, name) \
+#define QUTILITY_REGISTER_OPTION_IMPL_REGISTER_FUNCTION_WITH_DEFAULT(type, name, cml_token, default, desc) \
+    void name##_register_func(osd_T &osd) const                                                            \
+    {                                                                                                      \
+        boost::shared_ptr<od_T> opt(new od_T(                                                              \
+            cml_token,                                                                                     \
+            boost::program_options::value<type>()->default_value(default),                                 \
+            desc));                                                                                        \
+        osd.add(opt);                                                                                      \
+    }
+
+#define QUTILITY_REGISTER_OPTION_IMPL_REGISTER_FUNCTION_REQUIRED(type, name, cml_token, desc) \
+    void name##_register_func(osd_T &osd) const                                               \
+    {                                                                                         \
+        boost::shared_ptr<od_T> opt(new od_T(                                                 \
+            cml_token,                                                                        \
+            boost::program_options::value<type>()->required(),                                \
+            desc));                                                                           \
+        osd.add(opt);                                                                         \
+    }
+
+#define QUTILITY_REGISTER_OPTION_IMPL_STUB(class_name, name) \
     const bool name##_stub = make_stub(                      \
         &class_name::name##_get_func,                        \
         &class_name::name##_register_func,                   \
         &class_name::name##_str_func,                        \
         &class_name::name##_json_insert_func);
 
-#define QUTILITY_SIM_REGISTER_OPTIONS(class_name, type, name, tag, default, desc)     \
-    QUTILITY_SIM_REGISTER_OPTIONS_VALUE_AND_FUNCTIONS(type, name, tag, default, desc) \
-    QUTILITY_SIM_REGISTER_OPTIONS_STUB(class_name, name)
+#define QUTILITY_REGISTER_OPTION_WITH_DEFAULT(class_name, type, name, tag, cml_token, default, desc)   \
+    QUTILITY_REGISTER_OPTION_IMPL_VALUE_AND_COMMON_FUNCTIONS(type, name, tag)                          \
+    QUTILITY_REGISTER_OPTION_IMPL_REGISTER_FUNCTION_WITH_DEFAULT(type, name, cml_token, default, desc) \
+    QUTILITY_REGISTER_OPTION_IMPL_STUB(class_name, name)
 
-#define QUTILITY_SIM_REGISTER_OPTIONS_INITIALIZE(class_name)                                    \
-private:                                                                                        \
-    QUTILITY_SIM_REGISTER_OPTIONS_VALUE_AND_FUNCTIONS(int, sample, "sample-do-not-use", 0, ""); \
-    using get_func_T = decltype(&class_name::sample_get_func);                                  \
-    using register_func_T = decltype(&class_name::sample_register_func);                        \
-    using str_func_T = decltype(&class_name::sample_str_func);                                  \
-    using json_insert_func_T = decltype(&class_name::sample_json_insert_func);                  \
-    std::vector<get_func_T> get_funcs_;                                                         \
-    std::vector<register_func_T> register_funcs_;                                               \
-    std::vector<str_func_T> str_funcs_;                                                         \
-    std::vector<json_insert_func_T> json_insert_funcs_;                                         \
-    bool make_stub(                                                                             \
-        get_func_T get_func,                                                                    \
-        register_func_T register_func,                                                          \
-        str_func_T str_func,                                                                    \
-        json_insert_func_T json_func)                                                           \
-    {                                                                                           \
-        get_funcs_.push_back(get_func);                                                         \
-        register_funcs_.push_back(register_func);                                               \
-        str_funcs_.push_back(str_func);                                                         \
-        json_insert_funcs_.push_back(json_func);                                                \
-        return true;                                                                            \
-    }                                                                                           \
-                                                                                                \
-public:                                                                                         \
-    void get_all(vm_T &vm)                                                                      \
-    {                                                                                           \
-        for (const auto &ele : get_funcs_)                                                      \
-        {                                                                                       \
-            std::invoke(ele, *this, vm);                                                        \
-        }                                                                                       \
-    }                                                                                           \
-    void register_all(osd_T &od)                                                                \
-    {                                                                                           \
-        for (const auto &ele : register_funcs_)                                                 \
-        {                                                                                       \
-            std::invoke(ele, *this, od);                                                        \
-        }                                                                                       \
-    }                                                                                           \
-    void print_all(std::ostream &os)                                                            \
-    {                                                                                           \
-        for (const auto &ele : str_funcs_)                                                      \
-        {                                                                                       \
-            auto ans = std::invoke(ele, *this);                                                 \
-            os << ans.first << " = " << ans.second << ", ";                                     \
-        }                                                                                       \
-        os << std::endl;                                                                        \
-    }                                                                                           \
-    void json_insert_all(jsobject_T &obj)                                                       \
-    {                                                                                           \
-        for (const auto &ele : json_insert_funcs_)                                              \
-        {                                                                                       \
-            std::invoke(ele, *this, obj);                                                       \
-        }                                                                                       \
+#define QUTILITY_REGISTER_OPTION_REQUIRED(class_name, type, name, tag, cml_token, desc)   \
+    QUTILITY_REGISTER_OPTION_IMPL_VALUE_AND_COMMON_FUNCTIONS(type, name, tag)             \
+    QUTILITY_REGISTER_OPTION_IMPL_REGISTER_FUNCTION_REQUIRED(type, name, cml_token, desc) \
+    QUTILITY_REGISTER_OPTION_IMPL_STUB(class_name, name)
+
+#define QUTILITY_REGISTER_OPTION_INITIALIZE(class_name)                        \
+private:                                                                       \
+    void sample_get_func(const vm_T &);                                        \
+    void sample_register_func(osd_T &) const;                                  \
+    std::pair<std::string, std::string> sample_str_func() const;               \
+    void sample_json_insert_func(jsobject_T &) const;                          \
+    using get_func_T = decltype(&class_name::sample_get_func);                 \
+    using register_func_T = decltype(&class_name::sample_register_func);       \
+    using str_func_T = decltype(&class_name::sample_str_func);                 \
+    using json_insert_func_T = decltype(&class_name::sample_json_insert_func); \
+    std::vector<get_func_T> get_funcs_;                                        \
+    std::vector<register_func_T> register_funcs_;                              \
+    std::vector<str_func_T> str_funcs_;                                        \
+    std::vector<json_insert_func_T> json_insert_funcs_;                        \
+    bool make_stub(                                                            \
+        get_func_T get_func,                                                   \
+        register_func_T register_func,                                         \
+        str_func_T str_func,                                                   \
+        json_insert_func_T json_func)                                          \
+    {                                                                          \
+        get_funcs_.push_back(get_func);                                        \
+        register_funcs_.push_back(register_func);                              \
+        str_funcs_.push_back(str_func);                                        \
+        json_insert_funcs_.push_back(json_func);                               \
+        return true;                                                           \
+    }                                                                          \
+                                                                               \
+public:                                                                        \
+    void get_all(vm_T &vm)                                                     \
+    {                                                                          \
+        for (const auto &ele : get_funcs_)                                     \
+        {                                                                      \
+            std::invoke(ele, *this, vm);                                       \
+        }                                                                      \
+    }                                                                          \
+    void register_all(osd_T &od)                                               \
+    {                                                                          \
+        for (const auto &ele : register_funcs_)                                \
+        {                                                                      \
+            std::invoke(ele, *this, od);                                       \
+        }                                                                      \
+    }                                                                          \
+    void print_all(std::ostream &os)                                           \
+    {                                                                          \
+        for (const auto &ele : str_funcs_)                                     \
+        {                                                                      \
+            auto ans = std::invoke(ele, *this);                                \
+            os << ans.first << " = " << ans.second << ", ";                    \
+        }                                                                      \
+        os << std::endl;                                                       \
+    }                                                                          \
+    void json_insert_all(jsobject_T &obj)                                      \
+    {                                                                          \
+        for (const auto &ele : json_insert_funcs_)                             \
+        {                                                                      \
+            std::invoke(ele, *this, obj);                                      \
+        }                                                                      \
     }
 
 #endif
--- test/cml_opt_test.cpp
+++ test/cml_opt_test.cpp
@@ -7,23 +7,22 @@
 // current project header
 #include "qutility/cml_opt.h"
 
-class TestSimOptions : public qutility::cml_opt::SimOptionsBase
+class TestOptions : public qutility::cml_opt::OptionsBase
 {
 public:
-    QUTILITY_SIM_REGISTER_OPTIONS_INITIALIZE(TestSimOptions);
-    QUTILITY_SIM_REGISTER_OPTIONS(TestSimOptions, std::string, str, "str", "test string", "test string");
-    QUTILITY_SIM_REGISTER_OPTIONS(TestSimOptions, double, d, "d", 0.1, "test double");
-    QUTILITY_SIM_REGISTER_OPTIONS(TestSimOptions, size_t, i, "i", 233, "test double");
-
+    QUTILITY_REGISTER_OPTION_INITIALIZE(TestOptions);
+    QUTILITY_REGISTER_OPTION_WITH_DEFAULT(TestOptions, std::string, str, "string", "str", "test string", "test string");
+    QUTILITY_REGISTER_OPTION_WITH_DEFAULT(TestOptions, double, d, "double", "d", 0.1, "test double");
+    QUTILITY_REGISTER_OPTION_WITH_DEFAULT(TestOptions, size_t, i, "integer", "i", 233, "test integer");
 };
 
 TEST(CmlOpt, ReadOptions)
 {
-    constexpr int test_argc=5;
-    const char *test_argv[test_argc]={"??not important", "--str=hahaha", "--i=42", "--d=0.233", "--help"};
-    TestSimOptions opts;
-    
-    qutility::cml_opt::obtain_cml_options(test_argc,test_argv, opts);
+    constexpr int test_argc = 5;
+    const char *test_argv[test_argc] = {"??not important", "--str=hahaha", "--i=42", "--d=0.233", "--help"};
+    TestOptions opts;
+
+    qutility::cml_opt::obtain_cml_options(test_argc, test_argv, opts);
     EXPECT_EQ(opts.str, std::string{"hahaha"});
     EXPECT_EQ(opts.i, 42);
     EXPECT_EQ(opts.d, 0.233);
